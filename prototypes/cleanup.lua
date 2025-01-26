91d97adae6b73f059efc717b467cb4443f83c1a7
pla <massenpost@pla1.de>
2025-01-26 17:14:19 +0100

    added option for earlier fluid wagon

--- prototypes/cleanup.lua
+++ prototypes/cleanup.lua
@@ -1,3 +1,20 @@
--- cleanup some stuff in More Science
+-- cleanup tech tree
+local scienceNames    = require("__MoreScience__/prototypes/settings").scienceNames
 
 -- rocket cauldron
+
+
+if settings.startup["msr-early-fluid-wagon"].value == true then
+  LSlib.technology.removeIngredient("fluid-wagon",string.format(scienceNames.cyan, "pack"))
+  LSlib.technology.removeIngredient("fluid-wagon",string.format(scienceNames.blue, "pack"))
+  LSlib.technology.removeIngredient("fluid-wagon",string.format(scienceNames.orange, "pack"))
+
+  LSlib.technology.removePrerequisite("fluid-wagon", "fluid-handling-3")
+
+  
+  LSlib.technology.removeIngredient("automated-rail-transportation",string.format(scienceNames.orange, "pack"))
+  LSlib.technology.removePrerequisite("automated-rail-transportation", string.format(scienceNames.orange, "pack"))
+
+  LSlib.technology.addPrerequisite("braking-force-1", string.format(scienceNames.orange, "pack"))
+
+end
