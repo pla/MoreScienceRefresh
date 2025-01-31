ca6447fd1e916ec3cbad2792dd8bb9cb9107ff4a
pla <massenpost@pla1.de>
2025-01-31 11:37:27 +0100

    added orange science

--- prototypes/thermal-plant.lua
+++ prototypes/thermal-plant.lua
@@ -182,7 +182,7 @@ data:extend({
       ingredients = {
         {"automation-science-pack", 1},
         {"logistic-science-pack", 1},
-        -- {"advanced-automation-science-pack", 1},
+        {"advanced-automation-science-pack", 1},
         -- {"electric-power-science-pack", 1},
         -- {"chemical-science-pack", 1},
         -- {"production-science-pack", 1},
