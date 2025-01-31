f3635848bd41a4843c9b6124ba3f23abe195ef34
pla <massenpost@pla1.de>
2025-01-31 11:38:20 +0100

    rebalanced early fluid wagon

--- prototypes/cleanup.lua
+++ prototypes/cleanup.lua
@@ -7,14 +7,14 @@ local scienceNames    = require("__MoreScience__/prototypes/settings").scienceNa
 if settings.startup["msr-early-fluid-wagon"].value == true then
   LSlib.technology.removeIngredient("fluid-wagon",string.format(scienceNames.cyan, "pack"))
   LSlib.technology.removeIngredient("fluid-wagon",string.format(scienceNames.blue, "pack"))
-  LSlib.technology.removeIngredient("fluid-wagon",string.format(scienceNames.orange, "pack"))
 
   LSlib.technology.removePrerequisite("fluid-wagon", "fluid-handling-3")
+  LSlib.technology.addPrerequisite("fluid-wagon", "fluid-handling-2")
 
   
-  LSlib.technology.removeIngredient("automated-rail-transportation",string.format(scienceNames.orange, "pack"))
-  LSlib.technology.removePrerequisite("automated-rail-transportation", string.format(scienceNames.orange, "pack"))
+  -- LSlib.technology.removeIngredient("automated-rail-transportation",string.format(scienceNames.orange, "pack"))
+  -- LSlib.technology.removePrerequisite("automated-rail-transportation", string.format(scienceNames.orange, "pack"))
 
-  LSlib.technology.addPrerequisite("braking-force-1", string.format(scienceNames.orange, "pack"))
+  -- LSlib.technology.addPrerequisite("braking-force-1", string.format(scienceNames.orange, "pack"))
 
 end
--- settings-updates.lua
+++ settings-updates.lua
@@ -1,3 +1,4 @@
+-- make extended the default, unchangeable
 data.raw["bool-setting"]["MS-rocket-launching-extended"].hidden = true
 data.raw["bool-setting"]["MS-rocket-launching-extended"].allowed_values = {true}
 data.raw["bool-setting"]["MS-rocket-launching-extended"].default_value = true
